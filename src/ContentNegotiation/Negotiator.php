fab76f5e07d4e90023d17118c2507f81b01640ee
Mickael Vieira <contact@mickael-vieira.com>
2014-11-14 12:58:01 +0000

    Add Software license

--- src/ContentNegotiation/Negotiator.php
+++ src/ContentNegotiation/Negotiator.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of ContentNegotiation, a php implementation
+ * of the server driven negotiation
+ *
+ * (c) Mickaël Vieira <contact@mickael-vieira.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace ContentNegotiation;
 
 /**
